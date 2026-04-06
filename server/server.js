7c98c01a53089f9d66cefc24b3674cbc728f9883
QAXI-HASHIM-ALI <qaxihashimali@gmail.com>
2026-04-06 16:31:39 +0500

    Added the home route in server file

--- server/server.js
+++ server/server.js
@@ -5,17 +5,20 @@ import productRoutes from "./routes/productRoutes.js";
 import "dotenv/config";
 import orderRouter from "./routes/Orderroutes.js";
 
+const app = express();
+// POrt
+const PORT = process.env.PORT || 5000;
+//Database
 connectDB();
 
-const app = express();
-
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 app.use("/api", productRoutes);
 app.use("/api", orderRouter);
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
